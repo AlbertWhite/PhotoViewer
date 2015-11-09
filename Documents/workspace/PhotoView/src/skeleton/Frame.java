b3b6be8f3b6429a81f81b097264e9d49c9fa3a56
Yuebai <xyb1114@gmail.com>
2015-11-09 10:04:54 +0100

    change the layout

--- Documents/workspace/PhotoView/src/skeleton/Frame.java
+++ Documents/workspace/PhotoView/src/skeleton/Frame.java
@@ -11,6 +11,8 @@ import java.awt.Image;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
@@ -28,24 +30,25 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JToggleButton;
-
 
 public class Frame {
 
 	private static JFrame frame;
 	private static PhotoViewer imageViewer;
+	private static JToggleButton toggleButton = null;
 	private static boolean hasImage = false;// whether there is an image
 	static boolean flipImage = false;// whether the image has been flipped
 	static int width, height;
 	private static String path;
 	private static FreeHand freeHand = new FreeHand();
-	
+
 	private static void addPhotoViewerComponent(JFileChooser fc) {
 		path = fc.getSelectedFile().getAbsolutePath();// get the path of
-		
+
 		// if there already exists image, then the old image should be deleted
 		if (hasImage) {
 			frame.remove(imageViewer);
@@ -53,63 +56,57 @@ public class Frame {
 
 		imageViewer = new PhotoViewer(path);
 		hasImage = true;
-		//JPanel p = new ScrollImageTest();
-		//frame.add(p);
-		frame.add(imageViewer, BorderLayout.CENTER);
+		JPanel p = new ScrollImageTest(path);
+		frame.add(p);
+		// frame.add(imageViewer, BorderLayout.CENTER);
 		frame.pack();
 		frame.setVisible(true);
-		
+
 	}
 
 	private static void createAndShowGUI() {
 		frame = new JFrame("Photo Viewer");
-		frame.setSize(1000, 1000);
-		frame.setMinimumSize(new Dimension(1000,1000));
+		frame.setSize(200, 200);
+		// frame.setMinimumSize(new Dimension(1000,1000));
 
 		JPanel titlePanel = new JPanel();
 
-		// JMenuBar includes JMenu, and JMenu includes JMenuItem
+		// title menu
 		JMenu fileManu = new JMenu("File");
 		JMenuItem importImage = new JMenuItem("Import");
 		JMenuItem delete = new JMenuItem("Delete");
 		JMenuItem quit = new JMenuItem("Quit");
-		JMenuBar fileMenuBar = new JMenuBar();
-
 		fileManu.add(importImage);
 		fileManu.add(delete);
 		fileManu.add(quit);
 
+		JMenu modeMenu = new JMenu("Mode");
+		JRadioButtonMenuItem photoviewer = new JRadioButtonMenuItem("Photo Viewer", true);
+		JRadioButtonMenuItem browser = new JRadioButtonMenuItem("Browser");
+		JRadioButtonMenuItem split = new JRadioButtonMenuItem("Split Mode");
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(photoviewer);
+		bg.add(browser);
+		bg.add(split);
+		modeMenu.add(photoviewer);
+		modeMenu.add(browser);
+		modeMenu.add(split);
+		
+		JMenuBar fileMenuBar = new JMenuBar();
+
 		fileMenuBar.add(fileManu);
-		fileMenuBar.setPreferredSize(new Dimension(1000, 25));
-
-		JRadioButton photoViewer = new JRadioButton("Photo Viewer");
-		JRadioButton browser = new JRadioButton("Browser");
-		JRadioButton splitmode = new JRadioButton("Split Mode");
-
-		JLabel viewMode = new JLabel("     View Mode |");
-		fileMenuBar.add(viewMode);
-		fileMenuBar.add(photoViewer);
-		fileMenuBar.add(browser);
-		fileMenuBar.add(splitmode);
-
-		titlePanel.add(fileMenuBar);
+		fileMenuBar.add(modeMenu);
+		fileMenuBar.add(getColdWarmButton());
 
 		// status bar
 		JPanel statusPanel = new JPanel();
 		JLabel label = new JLabel("Status Bar");
 		statusPanel.add(label);
 
-		// sidebar bar
-		JPanel sidePanel = new JPanel();
-		JToggleButton family = new JToggleButton("Family");
-		JToggleButton vacation = new JToggleButton("Vacation");
-		sidePanel.add(family);
-		sidePanel.add(vacation);
-
 		frame.setLayout(new BorderLayout());
-		frame.add(titlePanel, BorderLayout.NORTH);
+		//
+		frame.add(fileMenuBar, BorderLayout.NORTH);
 		frame.add(statusPanel, BorderLayout.SOUTH);
-		frame.add(sidePanel, BorderLayout.WEST);
 		frame.setVisible(true);// if not, frame won't show up
 
 		// action listener on the button of import image
@@ -125,34 +122,33 @@ public class Frame {
 
 			}
 		});
-		
+
 		frame.addMouseListener(new MouseListener() {
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-								
+
 				if (hasImage) {
-					if(!flipImage){
-						
-				        DrawingListener listener = new DrawingListener(freeHand);
-				        freeHand.addMouseListener(listener);
-				        freeHand.addMouseMotionListener(listener);
-				        
-				        freeHand.addMouseListener(new MouseListener() {
+					if (!flipImage) {
+
+						DrawingListener listener = new DrawingListener(freeHand);
+						freeHand.addMouseListener(listener);
+						freeHand.addMouseMotionListener(listener);
+
+						freeHand.addMouseListener(new MouseListener() {
 
 							@Override
 							public void mouseClicked(MouseEvent e) {
 								// TODO Auto-generated method stub
-								System.out.println(e.getY()+e.getX());
-								
-								
+								System.out.println(e.getY() + e.getX());
+
 								JTextField text = new JTextField("click to input");
 								freeHand.setLayout(null);
 								freeHand.add(text);
 								Dimension size = text.getPreferredSize();
 								text.setBorder(javax.swing.BorderFactory.createEmptyBorder());
-								text.setBounds(e.getX(),e.getY(),100,20);
+								text.setBounds(e.getX(), e.getY(), 100, 20);
 								frame.pack();
 								frame.repaint();
 							}
@@ -160,29 +156,29 @@ public class Frame {
 							@Override
 							public void mousePressed(MouseEvent e) {
 								// TODO Auto-generated method stub
-								
+
 							}
 
 							@Override
 							public void mouseReleased(MouseEvent e) {
 								// TODO Auto-generated method stub
-								
+
 							}
 
 							@Override
 							public void mouseEntered(MouseEvent e) {
 								// TODO Auto-generated method stub
-								
+
 							}
 
 							@Override
 							public void mouseExited(MouseEvent e) {
 								// TODO Auto-generated method stub
-								
+
 							}
-				        	
-				        });
-						
+
+						});
+
 						try {
 							height = ImageIO.read(new File(path)).getHeight();
 							width = ImageIO.read(new File(path)).getWidth();
@@ -191,31 +187,30 @@ public class Frame {
 							e1.printStackTrace();
 						}
 
-						System.out.println(height+" "+width);
+						System.out.println(height + " " + width);
 						frame.remove(imageViewer);
 						flipImage = true;
 						imageViewer = new PhotoViewer(path);
-						//frame.add(imageViewer, BorderLayout.CENTER);
-						frame.add(freeHand,BorderLayout.CENTER);
+						// frame.add(imageViewer, BorderLayout.CENTER);
+						frame.add(freeHand, BorderLayout.CENTER);
 						frame.pack();
-						frame.repaint();//repaint is refresh the current interface
-						
+						frame.repaint();// repaint is refresh the current
+										// interface
 
-
-						
-					}else{
+					} else {
 						frame.remove(freeHand);
 						frame.remove(imageViewer);
 						flipImage = false;
 						imageViewer = new PhotoViewer(path);
 						frame.add(imageViewer, BorderLayout.CENTER);
-						frame.pack();//pack is necessary after adding the widget to the frame
+						frame.pack();// pack is necessary after adding the
+										// widget to the frame
 						frame.repaint();
-						
+
 					}
-				
+
 				}
-				
+
 			}
 
 			@Override
@@ -232,7 +227,7 @@ public class Frame {
 
 			@Override
 			public void mouseExited(MouseEvent e) {
-				
+
 			}
 		});
 	}
@@ -245,4 +240,25 @@ public class Frame {
 
 	}
 
+	private static JToggleButton getColdWarmButton() {
+		if (toggleButton == null) {
+			toggleButton = new JToggleButton();
+			// coldWarmButton.setMinimumSize(coldWarmButton.getPreferredSize());
+			// coldWarmButton.setPreferredSize(new Dimension(59, 59));
+
+			toggleButton.setText("School");
+			ItemListener coldWarmListener = new ItemListener() {
+				public void itemStateChanged(ItemEvent e) {
+					if (toggleButton.isSelected()) {
+						toggleButton.setText("Vacation");
+					} else {
+						toggleButton.setText("School");
+					}
+				}
+			};
+			toggleButton.addItemListener(coldWarmListener);
+		}
+		return toggleButton;
+
+	}
 }
